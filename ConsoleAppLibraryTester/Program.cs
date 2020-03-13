d2488b9d11f7f6c62ecd21282365a30e6993c982
Dave Mateer <davemateer@gmail.com>
2020-03-13 15:20:09 +0000

    Add project files.

---
+++ ConsoleAppLibraryTester/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MethodExtensions;
+
+namespace ConsoleAppLibraryTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string thing = "true";
+            bool isPossibleToConvert = thing.CanConvertToBoolean();
+
+            Console.WriteLine($"result is {isPossibleToConvert}");
+
+        }
+    }
+}
